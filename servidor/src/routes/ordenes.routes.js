12c93b2d25da69345e783e4cb499af7d61aaec54
emuller1996 <stefano.muller@hotmail.com>
2025-05-05 22:00:37 -0500

    ADD WEBHOOKS

--- servidor/src/routes/ordenes.routes.js
+++ servidor/src/routes/ordenes.routes.js
@@ -250,4 +250,53 @@ OrdenesRouters.get("/:id", async (req, res) => {
   }
 });
 
+OrdenesRouters.post("/webhooks", async (req, res) => {
+  console.log(req.body);
+  const data = req.body;
+  if (data.action === "payment.updated") {
+    try {
+      const payment_mercado = (
+        await axios.get(
+          `https://api.mercadopago.com/v1/payments/${data.data.id}`,
+          {
+            headers: { Authorization: `Bearer ${process.env.ACCESS_TOKEN}` },
+          }
+        )
+      ).data;
+      console.log(payment_mercado);
+      let pagoDatos ={
+        status: payment_mercado.status,
+        net_received_amount:
+          payment_mercado?.transaction_details?.net_received_amount,
+        net_amount: payment_mercado.transaction_amount,
+        fee_details_amount: payment_mercado?.fee_details?.[0]?.amount,
+        net_amount: payment_mercado?.net_amount,
+        status_detail: payment_mercado?.status_detail,
+      };
+      const response = await crearElasticByType(pagoDatos, "pago");
+      /* const pago = await Payment.update(
+        {
+          status: payment_mercado.status,
+          net_received_amount:
+            payment_mercado?.transaction_details?.net_received_amount,
+          net_amount: payment_mercado.transaction_amount,
+          fee_details_amount: payment_mercado?.fee_details?.[0]?.amount,
+          net_amount: payment_mercado?.net_amount,
+          status_detail: payment_mercado?.status_detail,
+        },
+        { where: { id_pago_merca: data.data.id } }
+      ); */
+
+      if(payment_mercado.status==="approved"){
+        //Buscar Orden y productos para dar de bajar el inventario 
+        //Enviar email del pago 
+      }
+      console.log(pago);
+    } catch (error) {
+      console.log(error);
+    }
+    return res.status(200).json({});
+  }
+});
+
 export default OrdenesRouters;
