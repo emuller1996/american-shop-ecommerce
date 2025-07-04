cc79875d3098f472145b0dce3bab95f9efec1d50
emuller1996 <stefano.muller@hotmail.com>
2025-07-03 23:01:03 -0500

    SE AGREGA COMPRIMIDOR DE IMAGENES

--- cliente/src/views/productos/ImagesPage/ImagesPage.js
+++ cliente/src/views/productos/ImagesPage/ImagesPage.js
@@ -9,6 +9,7 @@ import {
 } from '../../../services/productos.services'
 import toast from 'react-hot-toast'
 import PropTypes from 'prop-types'
+import imageCompression from 'browser-image-compression'
 
 const ImagesPage = ({ idProduct }) => {
   ImagesPage.propTypes = {
@@ -41,14 +42,25 @@ const ImagesPage = ({ idProduct }) => {
     }
   }
 
-  const handleImageChange = (e) => {
+  const handleImageChange = async (e) => {
     try {
       setSelectedImage(null)
       setErrorFile(null)
       const file = e.target.files[0]
       if (!file) return
 
-      // Validaciones
+      const options = {
+        maxSizeMB: 1, // Tamaño máximo en MB
+        maxWidthOrHeight: 1920, // Dimensión máxima
+        useWebWorker: true, // Usar web worker para mejor rendimiento
+        fileType: 'image/webp', // Convertir a WebP
+        initialQuality: 0.8, // Calidad (0.8 = 80%)
+      }
+
+      const compressedFile = await imageCompression(file, options)
+      console.log('Tamaño original:', file.size, 'bytes')
+      console.log('Tamaño comprimido:', compressedFile.size, 'bytes')
+      /* // Validaciones
       const validTypes = ['image/jpeg', 'image/jpg', 'image/png']
       const maxSize = 3 * 1024 * 1024 // 3MB
 
@@ -58,10 +70,10 @@ const ImagesPage = ({ idProduct }) => {
 
       if (file.size > maxSize) {
         throw new Error('El archivo excede el límite de 3MB')
-      }
-      if (file) {
-        setSelectedImage(file)
-        convertToBase64(file)
+      } */
+      if (compressedFile) {
+        setSelectedImage(compressedFile)
+        convertToBase64(compressedFile)
       }
     } catch (error) {
       console.error('Error al procesar imagen:', error.message)
@@ -71,6 +83,14 @@ const ImagesPage = ({ idProduct }) => {
       //alert(error.message)
       setErrorFile(error.message)
     }
+  }
+  const convertToBase642 = (file) => {
+    return new Promise((resolve, reject) => {
+      const reader = new FileReader()
+      reader.readAsDataURL(file)
+      reader.onload = () => resolve(reader.result)
+      reader.onerror = (error) => reject(error)
+    })
   }
   const convertToBase64 = (file) => {
     const reader = new FileReader()
