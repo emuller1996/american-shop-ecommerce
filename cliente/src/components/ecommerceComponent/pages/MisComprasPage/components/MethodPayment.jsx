53110e757f8ed946e5c041ae3fa0d53d5c00510a
emuller1996 <stefano.muller@hotmail.com>
2026-09-02 17:12:04 -0500

    feat : se agrega nueva svg para el metodo de pago wompi.

--- cliente/src/components/ecommerceComponent/pages/MisComprasPage/components/MethodPayment.jsx
+++ cliente/src/components/ecommerceComponent/pages/MisComprasPage/components/MethodPayment.jsx
@@ -3,6 +3,8 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import nequi_log from './../assets/nequi-colombia-svgrepo-com.svg'
 import tarjeta_log from './../assets/cards-outline-svgrepo-com.svg'
+import wompi_logo from './../assets/wompi_logo.svg'
+
 import "./MethodPayment.css"
 MethodPayment.propTypes = {
   payment_method: PropTypes.string,
@@ -30,7 +32,7 @@ export default function MethodPayment({ payment_method }) {
       {payment_method === 'Wompi' && (
         <>
           <div>
-            <img src={tarjeta_log} />
+            <img style={{width:"110px"}} src={wompi_logo} />
             <span>{payment_method}</span>
           </div>
         </>
