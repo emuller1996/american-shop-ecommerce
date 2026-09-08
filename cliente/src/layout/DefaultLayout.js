85fa6c991c95223a66ad638ace75bb831a2e7b7b
emuller1996 <stefano.muller@hotmail.com>
2026-09-07 23:46:28 -0500

    se agrega seo a la pagina de administracion del ecomerce

--- cliente/src/layout/DefaultLayout.js
+++ cliente/src/layout/DefaultLayout.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import { AppContent, AppSidebar, AppFooter, AppHeader } from '../components/index'
 import { Toaster } from 'react-hot-toast'
+import Seo from '../components/Seo'
 import './DefaultLayout.css'
 
 const DefaultLayout = () => {
   return (
     <div>
+      <Seo noindex title="Panel Administrativo" />
       <AppSidebar />
       <div className="wrapper d-flex flex-column min-vh-100">
         <AppHeader />
